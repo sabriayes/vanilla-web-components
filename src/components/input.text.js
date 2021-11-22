9a55f4b8cb95012ad42d82599ebf66430dc80627
Sabri Ayes <sabri@naylalabs.com>
2021-11-23 01:14:41 +0300

    Add attr and props, update init function.

--- src/components/input.text.js
+++ src/components/input.text.js
@@ -3,92 +3,193 @@ import templateHTML from './input.text.html';
 const template = document.createElement('template');
 template.innerHTML = templateHTML;
 
+/**
+ * CSS classes name.
+ * @property {string} HAS_LABEL - Element has a label.
+ * @property {string} HAS_VALUE - Input contains truly value.
+ * @readonly
+ */
+const CLASS = {
+    HAS_LABEL: 'has-label',
+    HAS_VALUE: 'has-value'
+}
+
+/**
+ * Custom component attribues.
+ * @property {string} LABEL         - Label attribute alias.
+ * @property {string} PLACEHOLDER   - Placeholder attribute alias.
+ * @readonly
+ */
+const ATTRS = {
+    LABEL: 'label',
+    PLACEHOLDER: 'placeholder'
+}
+
+/**
+ * Event list.
+ * @property {string} CHANGE - Change event alias.
+ * @readonly
+ */
+ const EVENTS = {
+     CHANGE: 'change'
+}
+
+/**
+ * Custom text input element class.
+ * @class
+ * @extends {HTMLElement}
+ * @constructor
+ */
 class InputText extends HTMLElement {
+    
+    /**
+     * style tage element in template.
+     * @property {CSSStyleSheet} $style
+     * @public
+     */
+    $style; 
+
+    /**
+     * ID #root-element in template.
+     * @property {HTMLElement} $root
+     * @public
+     */
+    $root;
+
+    /**
+     * ID #inner-container-element in template.
+     * @property {HTMLElement} $innerContainer
+     * @public
+     */
+    $innerContainer;
+
+    /**
+     * ID #input-element in template.
+     * @property {HTMLElement} $input
+     * @input
+     */
+    $input;
+
+    /**
+     * ID #label-container-element in template.
+     * @property {HTMLElement} $labelContainer
+     * @public
+     */
+    $labelContainer;
+
+    /**
+     * ID #label-element in template.
+     * @property {HTMLElement} $innerContainer
+     * @public
+     */
+    $label;
+    
+    get label() {
+        return this.getAttribute(ATTRS.LABEL);
+    }
+    set label(value) {
+        this.setAttribute(ATTRS.LABEL, value);
+    }
 
     get placeholder() {
-        return this.getAttribute('placeholder') || ''
+        return this.getAttribute(ATTRS.PLACEHOLDER);
+    }
+    set placeholder(value) {
+        this.setAttribute(ATTRS.PLACEHOLDER, value);
     }
 
-    get label() {
-        return this.getAttribute('label') || ''
+    static get observedAttributes() {
+        return Object.values(ATTRS);
     }
-
+      
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
-        this.render();
+        this.init();
     }
 
-    render() {
+    attributeChangedCallback(name, value, newValue) {
 
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        if(value === null) return;
 
-        const root = this.shadowRoot.querySelector('#root-element');
-        const input = this.shadowRoot.querySelector('#input-element');
-        const label = this.shadowRoot.querySelector('#label-element');
-        const labelContainer = this.shadowRoot.querySelector('#label-container');
-        const elements = {
-            root,
-            input,
-            labelContainer,
-            label
-        };
+        switch (name) {
 
-        this
-            .initStates(elements)
-            .initValues(elements)
-            .attachEvents(elements);
-        
-    }
+            case ATTRS.LABEL:
+                this.generateLabelElement(newValue);
+            break;
 
-    initStates(elements) {
-        
-        const HAS_LABEL = 'has-label';
-        const labelText = this.label;
-
-        elements.root.classList.remove(HAS_LABEL);
-        if(Boolean(labelText)) {
-            elements.root.classList.add(HAS_LABEL);
+            case ATTRS.PLACEHOLDER:
+                this.$input.setAttribute(
+                    ATTRS.PLACEHOLDER,
+                    newValue
+                );
+            break;
         }
-        else {
-            elements.labelContainer.remove();
-        }
-
-        return this;
     }
 
-    /**
-     * Function set initial value all defined elements.
-     * @param {*} elements 
-     * @returns {InputText}
-     */
-    initValues(elements) {
-
-        elements.input.setAttribute(
-            'placeholder', 
-            this.placeholder
-        );
-
-        elements.label.innerHTML = this.label
-
-        return this;
-    }
-
-    /**
-     * Function set events all defined elements.
-     * @param {*} elements 
-     * @returns {InputText}
-     */
-    attachEvents(elements) {
-
-        elements.input.addEventListener(
-            'change', 
+    disconnectedCallback() {
+        this.$input?.removeEventListener(
+            EVENTS.CHANGE, 
             this.eventInputChange.bind(this)
         );
-        return this;
+    }
+
+    init() {
+        
+        const clondedTemplate = template.content.cloneNode(true);
+        
+        this.$style = clondedTemplate.querySelector('style');
+        this.$root = clondedTemplate.getElementById('root-element');
+        this.$innerContainer = clondedTemplate.getElementById('inner-container-element');
+        this.$input = clondedTemplate.getElementById('input-element');
+        this.$label = clondedTemplate.getElementById('label-element');
+        this.$labelContainer = clondedTemplate.getElementById('label-container-element');
+
+        const _label = this.getAttribute(ATTRS.LABEL);
+        this.$labelContainer.remove();
+        this.generateLabelElement(_label);
+
+        const _placeholder = this.getAttribute(ATTRS.PLACEHOLDER);
+        this.$input.setAttribute(ATTRS.PLACEHOLDER, _placeholder);
+        this.$input.addEventListener(
+            EVENTS.CHANGE, 
+            this.eventInputChange.bind(this)
+        );
+
+        this.shadowRoot.appendChild(this.$style);
+        this.shadowRoot.appendChild(this.$root);
+    }
+
+    /**
+     * Update label container element state.
+     * If value is falsy remove label conatiner
+     * in root.
+     * @param {*} value - Label text value.
+     * @returns{void}
+     */
+    generateLabelElement(value) {
+
+        if(Boolean(value)) {
+
+            this.$root.classList.add(CLASS.HAS_LABEL);
+            this.$innerContainer.insertBefore(
+                this.$labelContainer,
+                this.$innerContainer.firstChild
+            );
+
+            this.$labelContainer = this.$innerContainer
+                .querySelector('#label-container-element');
+            this.$label = this.$innerContainer
+                .querySelector('#label-element');
+            this.$label.innerHTML = value;    
+        }
+        else {
+            this.$root.classList.remove(CLASS.HAS_LABEL);
+            this.$labelContainer.remove();
+        }
     }
     
     /**
@@ -100,17 +201,12 @@ class InputText extends HTMLElement {
      * @return {void}
      */
     eventInputChange($event) {
-        
-        const HAS_VALUE = 'has-value';
+
         const value = $event.target.value;
-        const root = this.shadowRoot.querySelector('#root-element');
-
         Boolean(value) ? 
-            root.classList.add(HAS_VALUE) : 
-            root.classList.remove(HAS_VALUE);
+            this.$root.classList.add(CLASS.HAS_VALUE) : 
+            this.$root.classList.remove(CLASS.HAS_VALUE);
     }
-
-    
 }
 
 window.customElements.define('input-text', InputText);
