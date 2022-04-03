72c864ff8cef81fd382b8b0b67d044692d606a88
Sabri Ayes <sabri@naylalabs.com>
2022-04-03 14:43:04 +0300

    Update some scripts and styles.

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -2,14 +2,14 @@ import HTML from './input.html';
 import CSS from './input.scss';
 
 /**
- * @description Template contents.
+ * HTML template content.
  * @var {HTMLElement} template
  */
 const template = document.createElement('template');
 template.innerHTML = HTML;
 
 /**
- * @description CSS classes name
+ * CSS classes.
  * @property {string} hasLabel    - Element has a label
  * @property {string} hasValue    - Input contains truly value
  * @property {string} init        - Element init completed
@@ -20,11 +20,11 @@ const CLASSES = {
 	hasLabel: 'has-label',
 	hasValue: 'has-value',
 	pendingInit: 'pending-init',
-	init: 'initialized',
+	initialized: 'initialized',
 };
 
 /**
- * @description Custom component attribues
+ * All custom attributes.
  * @property {string} label       - Alias of label attr
  * @property {string} placeholder - Alias of placeholder attr
  * @property {string} value       - Alias of value attr
@@ -37,10 +37,10 @@ const ATTRS = {
 };
 
 /**
- * @description Event list.
- * @property {string} change     - Alias change event
- * @property {string} click      - Alias click event
- * @property {string} slotChange - Alias slot change event
+ * All event aliases.
+ * @property {string} change     - Alias of change event
+ * @property {string} click      - Alias of click event
+ * @property {string} slotChange - Alias of slot change event
  * @readonly
  */
 const EVENTS = {
@@ -50,16 +50,30 @@ const EVENTS = {
 };
 
 /**
- * Custom text input element classES.
- * HTML tags is <vanilla-input>
- *
+ * Returns a <style> element with appended styles.
+ * @param {string} cssText - CSS style sheet content.
+ * @return {HTMLElement}
+ */
+const getStyleElement = function (cssText) {
+	const styleElement = document.createElement('style');
+	if (styleElement.styleSheet) {
+		styleElement.styleSheet.cssText = cssText;
+		return styleElement;
+	}
+	const textNode = document.createTextNode(cssText);
+	styleElement.appendChild(textNode);
+	return styleElement;
+};
+
+/**
+ * Custom text input element class. HTML tag is <vanilla-input>
  * @class
  * @extends {HTMLElement}
  * @constructor
  */
 class VanillaInput extends HTMLElement {
 	/**
-	 * Tag name of VanillaInput
+	 * Tag name of custom element.
 	 * @property {string} tagName
 	 * @static
 	 */
@@ -103,7 +117,7 @@ class VanillaInput extends HTMLElement {
 	$label;
 
 	/**
-	 * Get current value.
+	 * Getter method for input value.
 	 * @return {string}
 	 */
 	get value() {
@@ -111,7 +125,7 @@ class VanillaInput extends HTMLElement {
 	}
 
 	/**
-	 * Set value.
+	 * Setter method for input value.
 	 * @return {void}
 	 */
 	set value(val) {
@@ -121,7 +135,7 @@ class VanillaInput extends HTMLElement {
 	}
 
 	/**
-	 * Get label property value.
+	 * Getter method for label property.
 	 * @return {string}
 	 */
 	get label() {
@@ -129,7 +143,7 @@ class VanillaInput extends HTMLElement {
 	}
 
 	/**
-	 * Set label property value.
+	 * Setter method for label property.
 	 * @param {string} value
 	 * @return {void}
 	 */
@@ -147,47 +161,47 @@ class VanillaInput extends HTMLElement {
 	}
 
 	/**
-	 * Invoked when change/update attriubtes that defined
+	 * Invoked when change/update attributes that defined
 	 * at observedAttributes() function.
 	 *
-	 * @param {string} name     - Attribute name.
-	 * @param {string} value    - Attribute current value.
-	 * @param {string} newValue - Attribute updated value.
+	 * @param {string} name     - Attribute name
+	 * @param {string} value    - Attribute current value
+	 * @param {string} newValue - Attribute updated value
 	 * @return {void}
 	 */
 	attributeChangedCallback(name, value, newValue) {
-		this.attributeChanged(name, value, newValue);
+		this.changeAttributeValue(name, value, newValue);
 	}
 
 	/**
 	 * Helper function that called by init() and
-	 * attributeChangedCallback().
+	 * attributeChangedCallback()
 	 *
-	 * @param {string} name     - Attribute name.
-	 * @param {string} value    - Attribute current value.
-	 * @param {string} newValue - Attribute updated value.
+	 * @param {string} name     - Attribute name
+	 * @param {string} value    - Attribute current value
+	 * @param {string} newValue - Attribute updated value
 	 * @return {void}
 	 */
-	attributeChanged(name, value, newValue) {
+	changeAttributeValue(name, value, newValue) {
 		if (!value) {
 			return;
 		}
 
 		switch (name) {
 			case ATTRS.label:
-				// Invoke when change label attr/property value.
-				this.generateLabelElement(newValue);
+				// Invoke when change value of label attr/property
+				this.updateLabelElement(newValue);
 				break;
 
-			case ATTRS.VALUE:
-				// Dispatch change event with fake event.
+			case ATTRS.value:
+				// Dispatch change event with fake event object
 				const fakeEvent = new Event(EVENTS.change);
-				this.$input?.setAttribute(name, newValue);
-				this.$input?.dispatchEvent(fakeEvent);
+				this.$input.setAttribute(name, newValue);
+				this.$input.dispatchEvent(fakeEvent);
 				break;
 
 			default:
-				this.$input?.setAttribute(name, newValue);
+				this.$input.setAttribute(name, newValue);
 		}
 	}
 
@@ -196,20 +210,20 @@ class VanillaInput extends HTMLElement {
 	}
 
 	disconnectedCallback() {
-		this.$input?.removeEventListener(
+		this.$input.removeEventListener(
 			EVENTS.change,
-			this.eventChangedInputValue.bind(this),
+			this._eventChangedInputValue.bind(this),
 		);
-		this.$root?.removeEventListener(
+		this.$root.removeEventListener(
 			EVENTS.click,
-			this.eventClickedToRoot.bind(this),
+			this._eventClickedToRoot.bind(this),
 		);
 	}
 
 	init() {
 		const clondedTemplate = template.content.cloneNode(true);
 
-		// Access cloned DOM elements.
+		// Access cloned DOM elements
 		this.$root = clondedTemplate.getElementById('root-element');
 		this.$innerContainer = clondedTemplate.getElementById(
 			'inner-container-element',
@@ -221,35 +235,26 @@ class VanillaInput extends HTMLElement {
 		);
 		this.$labelContainer.remove();
 
-		// Add events.
+		// Bind all event listeners
 		this.$input.addEventListener(
 			EVENTS.change,
-			this.eventChangedInputValue.bind(this),
+			this._eventChangedInputValue.bind(this),
 		);
 		this.$root.addEventListener(
 			EVENTS.click,
-			this.eventClickedToRoot.bind(this),
+			this._eventClickedToRoot.bind(this),
 		);
 
-		const initialAttributes = this.attributes;
-		for (const node of initialAttributes) {
-			this.attributeChanged(node.name, node.value, node.value);
+		for (const node of this.attributes) {
+			this.changeAttributeValue(node.name, node.value, node.value);
 		}
 
-		const css = document.createElement('style');
+		// Append styles to root element
+		this.shadowRoot.appendChild(getStyleElement(CSS));
 
-		if (css.styleSheet) {
-			css.styleSheet.cssText = CSS;
-		} else {
-			const textNode = document.createTextNode(CSS);
-			css.appendChild(textNode);
-		}
-
-		this.shadowRoot.appendChild(css);
 		this.shadowRoot.appendChild(this.$root);
-
 		this.$root.classList.remove(CLASSES.pendingInit);
-		this.$root.classList.add(CLASSES.init);
+		this.$root.classList.add(CLASSES.initialized);
 	}
 
 	/**
@@ -257,10 +262,10 @@ class VanillaInput extends HTMLElement {
 	 * If value is falsy remove label container
 	 * in root.
 	 *
-	 * @param {string} value - Label text value.
+	 * @param {string} value - Label text value
 	 * @returns {void}
 	 */
-	generateLabelElement(value) {
+	updateLabelElement(value) {
 		if (Boolean(value)) {
 			this.$root.classList.add(CLASSES.hasLabel);
 			this.$innerContainer.insertBefore(
@@ -273,10 +278,10 @@ class VanillaInput extends HTMLElement {
 			);
 			this.$label = this.$innerContainer.querySelector('#label-element');
 			this.$label.innerHTML = value;
-		} else {
-			this.$root.classList.remove(CLASSES.hasLabel);
-			this.$labelContainer.remove();
+			return;
 		}
+		this.$root.classList.remove(CLASSES.hasLabel);
+		this.$labelContainer.remove();
 	}
 
 	/**
@@ -287,13 +292,13 @@ class VanillaInput extends HTMLElement {
 	 * @param {Event} $event
 	 * @return {void}
 	 */
-	eventChangedInputValue($event) {
+	_eventChangedInputValue($event) {
 		const value = $event.target.value;
 		if (Boolean(value)) {
 			this.$root.classList.add(CLASSES.hasValue);
-		} else {
-			this.$root.classList.remove(CLASSES.hasValue);
+			return;
 		}
+		this.$root.classList.remove(CLASSES.hasValue);
 	}
 
 	/**
@@ -303,7 +308,7 @@ class VanillaInput extends HTMLElement {
 	 * @param {Event} $event
 	 * @return {void}
 	 */
-	eventClickedToRoot($event) {
+	_eventClickedToRoot($event) {
 		$event.preventDefault();
 		this.$input.focus();
 	}
---
+++ src/partials/js/classes.js
@@ -0,0 +1,14 @@
+/**
+ * @description CSS classes name
+ * @property {string} hasLabel    - Element has a label
+ * @property {string} hasValue    - Input contains truly value
+ * @property {string} init        - Element init completed
+ * @property {string} pendingInit - Element not initialized yet
+ * @readonly
+ */
+export const CLASSES = {
+	hasLabel: 'has-label',
+	hasValue: 'has-value',
+	pendingInit: 'pending-init',
+	init: 'initialized',
+};
