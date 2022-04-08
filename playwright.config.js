35560015a44e5bf77d0d7394bbcb2c7afbc00fd8
Sabri Ayes <sabri@naylalabs.com>
2022-04-08 13:30:49 +0300

    Add @playwrite/test dependency

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,8 @@ const { devices } = require('@playwright/test');
 
 const config = {
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
-	use: {
-		trace: 'on-first-retry',
-	},
+	retries: 0,
+	use: { trace: 'on-first-retry' },
 	projects: [
 		{
 			name: 'chromium',
